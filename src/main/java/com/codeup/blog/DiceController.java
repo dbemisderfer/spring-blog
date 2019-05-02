cbf905addec3f3215b94c67f6c45b9f104e5737a
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-05-02 13:26:36 -0500

    completion of roll-dice exercise from views lesson

--- src/main/java/com/codeup/blog/DiceController.java
+++ src/main/java/com/codeup/blog/DiceController.java
@@ -1,4 +1,25 @@
 package com.codeup.blog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class DiceController {
+
+    @GetMapping("/roll-dice")
+    public String diceGame() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String playDiceGame(@PathVariable int n, Model viewModel) {
+        int dieRoll = (int) ((Math.random() * 6) + 1);
+        viewModel.addAttribute("num", n);
+        viewModel.addAttribute("die", dieRoll);
+        return "roll-dice";
+    }
+
+
 }
