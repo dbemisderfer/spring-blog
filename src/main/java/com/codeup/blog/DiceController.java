6751ae743009e06c822f291b81aa7b178d08e747
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-05-06 14:58:32 -0500

    completion of dice exercise

--- src/main/java/com/codeup/blog/DiceController.java
+++ src/main/java/com/codeup/blog/DiceController.java
@@ -7,19 +7,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DiceController {
+    int dieRoll; // allows die roll to persist until correct die roll is chosen
 
     @GetMapping("/roll-dice")
     public String diceGame() {
+//        dieRoll = (int) Math.floor(Math.random() * 6 + 1);
+        dieRoll = (int) ((Math.random() * 6) + 1);
         return "roll-dice";
     }
 
     @GetMapping("/roll-dice/{n}")
     public String playDiceGame(@PathVariable int n, Model viewModel) {
-        int dieRoll = (int) ((Math.random() * 6) + 1);
         viewModel.addAttribute("num", n);
         viewModel.addAttribute("die", dieRoll);
-        return "roll-dice";
+        return "guess-dice";
     }
+
+//    @GetMapping("/roll-dice")
+//    public String diceGame() {
+//        return "roll-dice";
+//    }
+//
+//    @GetMapping("/roll-dice/{n}")
+//    public String playDiceGame(@PathVariable int n, Model viewModel) {
+////        int dieRoll = (int) ((Math.random() * 6) + 1);
+//        int dieRoll = (int) Math.floor(Math.random() * 6 + 1);
+//        viewModel.addAttribute("num", n);
+//        viewModel.addAttribute("die", dieRoll);
+//        return "roll-dice";
+//    }
 
 
 }
